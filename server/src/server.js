6cddd7034f9471c7e584e19b2bb3e63a747d5799
Cong Wang <cngwang6@gmail.com>
2016-11-29 01:19:48 -0500

    static

--- server/src/server.js
+++ server/src/server.js
@@ -1,23 +1,10 @@
 var express = require('express');
-var app=express();
-var poster = require('./util');
-var rev=poster.reverseString;
-var bodyParser = require('body-parser');
-app.use(bodyParser.text());
-app.get('/', function (req, res) {
-	res.send("hello world!");
+var app = express();
+// var poster = require('./util');
+// var rev = poster.reverseString;
+// var bodyParser = require('body-parser');
+app.use(express.static('../client/build'));
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000.');
 });
-app.listen(3000, function(){
-	console.log('Example app listening on port 3000.');
-});
-app.post('/reverse', function (req, res) {
-	 /* body... */ 
-	 //input, output somewhere in there
-	 if (typeof(req.body)==='string'){
-	 	var reversed=rev(req.body);
-	 	res.send(reversed);
-	 }
-	 else {
-	 	res.status(400).end();
-	 }
-})
+
