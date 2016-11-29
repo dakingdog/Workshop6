f6fd7016fb4f054b380fec411e26c4ceaa450241
Cong Wang <cngwang6@gmail.com>
2016-11-29 16:22:14 -0500

    validation

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,95 @@
 var express = require('express');
 var app = express();
-// var poster = require('./util');
-// var rev = poster.reverseString;
-// var bodyParser = require('body-parser');
+var validate = require('express-jsonschema').validate;
+var StatusUpdateSchema = require('./schemas/statusupdate.json');
+var bodyParser = require('body-parser');
 app.use(express.static('../client/build'));
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000.');
-});
 var docFunctions = require('./database');
 var readDocument = docFunctions.readDocument;
+var writeDocument = docFunctions.writeDocument;
+var addDocument = docFunctions.addDocument;
+// Support receiving text in HTTP request bodies
+app.use(bodyParser.text());
+// Support receiving JSON in HTTP request bodies
+app.use(bodyParser.json())
+// userid getter from token. -1 if failure.
+function getUserIdFromToken(authorizationLine) {
+  try {
+    var token = authorizationLine.slice(7);
+    var regularString = new Buffer(token, 'base64').toString('utf8');
+    var tokenObj = JSON.parse(regularString);
+    var id = tokenObj['id'];
+    if (typeof (id) === 'number') {
+      return id;
+    } else {
+      return -1;
+    }
+  } catch ( e ) {
+    // statements
+    return -1;
+  // console.log(e);
+  }
+}
+function postStatusUpdate(user, location, contents) {
+  // If we were implementing this for real on an actual server, we would check
+  // that the user ID is correct & matches the authenticated user. But since
+  // we're mocking it, we can be less strict.
+  // Get the current UNIX time.
+  var time = new Date().getTime();
+  // The new status update. The database will assign the ID for us.
+  var newStatusUpdate = {
+    "likeCounter": [],
+    "type": "statusUpdate",
+    "contents": {
+      "author": user,
+      "postDate": time,
+      "location": location,
+      "contents": contents,
+      "likeCounter": []
+    },
+    // List of comments on the post
+    "comments": []
+  };
+  // Add the status update to the database.
+  // Returns the status update w/ an ID assigned.
+  newStatusUpdate = addDocument('feedItems', newStatusUpdate);
+  // Add the status update reference to the front of the current user's feed.
+  var userData = readDocument('users', user);
+  var feedData = readDocument('feeds', userData.feed);
+  feedData.contents.unshift(newStatusUpdate._id);
+  // Update the feed object.
+  writeDocument('feeds', feedData);
+  // Return the newly-posted object.
+  return newStatusUpdate;
+}
+// `POST /feeditem { userId: user, location: location, contents: contents }`
+app.post('/feeditem',
+  validate({
+    body: StatusUpdateSchema
+  }), function(req, res) {
+    // If this function runs, `req.body` passed JSON validation!
+    var body = req.body;
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
+    // Check if requester is authorized to post this status update.
+    // (The requester must be the author of the update.)
+    if (fromUser === body.userId) {
+      var newUpdate = postStatusUpdate(body.userId, body.location,
+        body.contents);
+      // When POST creates a new resource, we should tell the client about it
+      // in the 'Location' header and use status code 201.
+      res.status(201);
+      res.set('Location', '/feeditem/' + newUpdate._id);
+      // Send the update!
+      res.send(newUpdate);
+    } else {
+      // 401: Unauthorized.
+      res.status(401).end();
+    }
+  });
+  /**
+  * Adds a new status update to the database.
+  */
+
 function getFeedItemSync(feedItemId) {
   var feedItem = readDocument('feedItems', feedItemId);
   // Resolve 'like' counter.
@@ -48,21 +129,15 @@ app.get('/user/:userid/feed', function(req, res) {
 //send response:
 // res.send(getFeedData(userid));
 });
-// userid getter from token. -1 if failure.
-function getUserIdFromToken(authorizationLine) {
-  try {
-    var token = authorizationLine.slice(7);
-    var regularString = new Buffer(token, 'base64').toString('utf8');
-    var tokenObj = JSON.parse(regularString);
-    var id = tokenObj['id'];
-    if (typeof (id) === 'number') {
-      return id;
-    } else {
-      return -1;
-    }
-  } catch ( e ) {
-    // statements
-    return -1;
-  // console.log(e);
+app.use(function(err, req, res, next) {
+  if (err.name === 'JsonSchemaValidation') {
+    // Set a bad request http response status
+    res.status(400).end();
+  } else {
+    // It's some other sort of error; pass it to next error middleware handler
+    next(err);
   }
-}
+});
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000.');
+});
