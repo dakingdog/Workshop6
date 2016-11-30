552bb23362085792174178638594f6d95f0f9dbd
Cong Wang <cngwang6@gmail.com>
2016-11-29 20:01:28 -0500

    almostDone

--- client/app/database.js
+++ client/app/database.js
@@ -203,9 +203,13 @@ export class ResetDatabase extends React.Component {
   render() {
     return (
       <button className="btn btn-default" type="button" onClick={() => {
-        resetDatabase();
-        window.alert("Database reset! Refreshing the page now...");
-        document.location.reload(false);
+        var xhr=new XMLHttpRequest();
+        xhr.open('POST', '/resetdb');
+        xhr.addEventListener('load', function () {
+           window.alert('database reset. Reloading page once you hit ok.');
+           document.location.reload(false); 
+        });
+        xhr.send()
       }}>Reset Mock DB</button>
     );
   }
--- server/src/server.js
+++ server/src/server.js
@@ -129,6 +129,131 @@ app.get('/user/:userid/feed', function(req, res) {
 //send response:
 // res.send(getFeedData(userid));
 });
+app.post('/resetdb', function(req, res) {
+  console.log('resetting db');
+  docFunctions.resetDatabase();
+  res.send();
+});
+app.put('/feeditem/:feeditemid/content', function(req, res) {
+  var fromUser = getUserIdFromToken(req.get('Authorization'));
+  var feedItemId = req.params.feeditemid;
+  var feedItem = readDocument('feedItems', feedItemId);
+  if (fromUser === feedItem.contents.author) {
+    if (typeof (req.body) !== 'string') {
+      res.status(400).end();
+      return;
+    }
+    feedItem.contents.contents = req.body;
+    writeDocument('feedItems', feedItem);
+    res.send(getFeedItemSync(feedItemId));
+  } else {
+    res.status(401).end();
+  }
+})
+/**
+* Delete a feed item.
+*/
+app.delete('/feeditem/:feeditemid', function(req, res) {
+  var fromUser = getUserIdFromToken(req.get('Authorization'));
+  // Convert from a string into a number.
+  var feedItemId = parseInt(req.params.feeditemid, 10);
+  var feedItem = readDocument('feedItems', feedItemId);
+  // Check that the author of the post is requesting the delete.
+  if (feedItem.contents.author === fromUser) {
+    docFunctions.deleteDocument('feedItems', feedItemId);
+    // Remove references to this feed item from all other feeds.
+    var feeds = docFunctions.getCollection('feeds');
+    var feedIds = Object.keys(feeds);
+    feedIds.forEach((feedId) => {
+      var feed = feeds[feedId];
+      var itemIdx = feed.contents.indexOf(feedItemId);
+      if (itemIdx !== -1) {
+        // Splice out of array.
+        feed.contents.splice(itemIdx, 1);
+        // Update feed.
+        writeDocument('feeds', feed);
+      }
+    });
+    // Send a blank response to indicate success.
+    res.send();
+  } else {
+    // 401: Unauthorized.
+    res.status(401).end();
+  }
+});
+// Like a feed item.
+app.put('/feeditem/:feeditemid/likelist/:userid', function(req, res) {
+  var fromUser = getUserIdFromToken(req.get('Authorization'));
+  // Convert params from string to number.
+  var feedItemId = parseInt(req.params.feeditemid, 10);
+  var userId = parseInt(req.params.userid, 10);
+  if (fromUser === userId) {
+    var feedItem = readDocument('feedItems', feedItemId);
+    // Add to likeCounter if not already present.
+    if (feedItem.likeCounter.indexOf(userId) === -1) {
+      feedItem.likeCounter.push(userId);
+      writeDocument('feedItems', feedItem);
+    }
+    // Return a resolved version of the likeCounter
+    res.send(feedItem.likeCounter.map((userId) => readDocument('users', userId)));
+  } else {
+    // 401: Unauthorized.
+    res.status(401).end();
+  }
+});
+// Unlike a feed item.
+app.delete('/feeditem/:feeditemid/likelist/:userid', function(req, res) {
+  var fromUser = getUserIdFromToken(req.get('Authorization'));
+  // Convert params from string to number.
+  var feedItemId = parseInt(req.params.feeditemid, 10);
+  var userId = parseInt(req.params.userid, 10);
+  if (fromUser === userId) {
+    var feedItem = readDocument('feedItems', feedItemId);
+    var likeIndex = feedItem.likeCounter.indexOf(userId);
+    // Remove from likeCounter if present
+    if (likeIndex !== -1) {
+      feedItem.likeCounter.splice(likeIndex, 1);
+      writeDocument('feedItems', feedItem);
+    }
+    // Return a resolved version of the likeCounter
+    // Note that this request succeeds even if the
+    // user already unliked the request!
+    res.send(feedItem.likeCounter.map((userId) => readDocument('users', userId)));
+  } else {
+    // 401: Unauthorized.
+    res.status(401).end();
+  }
+});
+// Search for feed item
+app.post('/search', function(req, res) {
+  var fromUser = getUserIdFromToken(req.get('Authorization'));
+  var user = readDocument('users', fromUser);
+  if (typeof (req.body) === 'string') {
+    // trim() removes whitespace before and after the query.
+    // toLowerCase() makes the query lowercase.
+    var queryText = req.body.trim().toLowerCase();
+    // Search the user's feed.
+    var feedItemIDs = readDocument('feeds', user.feed).contents;
+    // "filter" is like "map" in that it is a magic method for
+    // arrays. It takes an anonymous function, which it calls
+    // with each item in the array. If that function returns 'true',
+    // it will include the item in a return array. Otherwise, it will
+    // not.
+    // Here, we use filter to return only feedItems that contain the
+    // query text.
+    // Since the array contains feed item IDs, we later map the filtered
+    // IDs to actual feed item objects.
+    res.send(feedItemIDs.filter((feedItemID) => {
+      var feedItem = readDocument('feedItems', feedItemID);
+      return feedItem.contents.contents
+          .toLowerCase()
+          .indexOf(queryText) !== -1;
+    }).map(getFeedItemSync));
+  } else {
+    // 400: Bad Request.
+    res.status(400).end();
+  }
+});
 app.use(function(err, req, res, next) {
   if (err.name === 'JsonSchemaValidation') {
     // Set a bad request http response status
